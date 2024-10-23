888b7e7eb643c4b396b8b834a2dd2cde6a5c0782
Your Name <you@example.com>
2024-10-23 19:19:04 -0300

    fix: adjust in way to iteract vulnerabilities

--- dds_vulners_collect.py
+++ dds_vulners_collect.py
@@ -12,8 +12,12 @@ import time
 # ***** CONFIGURATION *****
 GEMINI_API_KEY = "GEMINI_API_KEY" # Replace with your Gemini API key
 VULNERS_API_KEY = "VULNERS_API_KEY" # Replace with your actual Vulners API key
-CSV_OUTPUT_FILE = "dds_vulnerabilities_AI.csv" # Change the output file name
+CSV_OUTPUT_FILE = "dataset/dds_vulnerabilities_AI.csv" # Change the output file name
 # ***** END CONFIGURATION *****
+
+# Define a chave de API do Gemini como uma variável de ambiente
+os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
+
 # Safety configuration for Gemini
 safe = [
     {
@@ -34,10 +38,9 @@ safe = [
     },
 ]
 
-
 VENDORS = [
     "TiTAN DDS", "CoreDX", "Core DX", "Zhenrong DDS", "MilDDS", "Mil DDS", "GurumDDS", "InterCOM",
-    "Fast DDS", "fastdds", "cyclonedds", "connext", "opendds", "data distribution service vulnerabilities"
+    "Fast DDS", "fastdds", "cyclonedds", "connext", "opendds"
 ]
 
 FIELDS = ["bulletinFamily", "cvss", "description", "id", "lastseen", "modified", "published", "title", "type", "vhref", "viewCount", "href", "enchantments", "bounty", "sourceData", "cvss3", "cvss2", "epss"]
@@ -54,7 +57,6 @@ def get_vulners_data(query, fields, skip=0):
     response = requests.post(base_url, data=json.dumps(data))
     response.raise_for_status()
     return response.json()
-
 
 def extract_vulners_data(vulnerability):
     """Extracts relevant information from a Vulners vulnerability entry, handling encoding."""
@@ -89,21 +91,20 @@ def extract_vulners_data(vulnerability):
         'description': truncated_description,
         'vendor': vendor,
         'published': vulnerability.get('_source', {}).get('published'),
-        'cvss': vulnerability.get('_source', {}).get('cvss', {}).get('score'), # vulners cvss is on score
+        'cvss_score': vulnerability.get('_source', {}).get('cvss', {}).get('score'), # vulners cvss is on score
         'severity': vulnerability.get('_source', {}).get('cvss', {}).get('severity'), # vulners severity is on severity
         'source': 'Vulners'
     }
 
 def categorize_vulnerability(description):
-    """Categorizes the vulnerability using Google Gemini."""
+    """Categorizes the vulnerability and extracts cause, impact, and vendor using Google Gemini."""
     genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
     model = genai.GenerativeModel('gemini-1.5-pro')
 
-
     prompt = f"""
-    You are a security expert. \
-    Categorize the following vulnerability description into a CWE category. \
-    Provide the CWE ID and a brief explanation.
+    You are a security expert.
+    Categorize the following vulnerability description into a CWE category, identify the vendor, and extract the cause and impact of the vulnerability.
+    Provide the CWE ID, a brief explanation, the vendor name, the cause of the vulnerability, and its impact.
 
     Description:
     ```
@@ -112,80 +113,118 @@ def categorize_vulnerability(description):
 
     Output:
     ```json
-    {{"cwe_category": "CWE-ID", "explanation": "Brief Explanation of the CWE"}}
+    {{"cwe_category": "CWE-ID", "explanation": "Brief Explanation of the CWE", "vendor": "Vendor Name", "cause": "Cause of the Vulnerability", "impact": "Impact of the Vulnerability"}}
     ```
     """
     time.sleep(1)
-    
+
     try:
         response = model.generate_content(prompt, safety_settings=safe)
         print(response)
-        if response._result.candidates:
 
+        if response._result.candidates:
             if response._result.candidates[0].content.parts:
                 text_part = response._result.candidates[0].content.parts[0].text
-                json_str = response._result.candidates[0].content.parts[0].text.strip().replace("```json\n", "").replace("\n```", "")
-                result = json.loads(json_str, strict=False)
-                return result['cwe_category'], result['explanation']
-            else:
-                print(f"Gemini response has no parts: {response._result}")
-                return "UNKNOWN", "Gemini response has no parts"
-        else:
-            print(f"Gemini returned no candidates: {response._result}")
-            return "UNKNOWN", "Gemini returned no candidates"
 
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON from Gemini response: {json_str}, Error: {e}")
-        return "UNKNOWN", "Error categorizing vulnerability"
+                # Extrair o JSON usando expressão regular
+                match = re.search(r'``[`json\n(.*?)\n`](command:_github.copilot.openSymbolFromReferences?%5B%22%22%2C%5B%7B%22uri%22%3A%7B%22scheme%22%3A%22file%22%2C%22authority%22%3A%22%22%2C%22path%22%3A%22%2Fd%3A%2FMESTRADO%2FDDS%2FSCRAP_DDS%2Fscrap_generate_dataset.py%22%2C%22query%22%3A%22%22%2C%22fragment%22%3A%22%22%7D%2C%22pos%22%3A%7B%22line%22%3A2%2C%22character%22%3A7%7D%7D%5D%2C%224136289f-5814-4b75-9d6f-56e9b5bfd10d%22%5D "Go to definition")``', text_part, re.DOTALL)
+                if match:
+                    json_str = match.group(1)
+                    
+                    try:
+                        result = json.loads(json_str, strict=False)
+                        cwe_category = result.get('cwe_category', 'Unknown')
+                        explanation = result.get('explanation', 'Could not categorize vulnerability')
+                        vendor = result.get('vendor', 'Unknown')
+                        cause = result.get('cause', 'Unknown')
+                        impact = result.get('impact', 'Unknown')
+                        return [(cwe_category, explanation, vendor, cause, impact)]
+                    except json.JSONDecodeError:
+                        print(f"Error parsing JSON from Gemini response: {json_str}")
+                        return [("UNKNOWN", "Error categorizing vulnerability", "Unknown", "Unknown", "Unknown")]
+                else:
+                    print(f"Could not find JSON in Gemini response: {text_part}")
+                    return [("UNKNOWN", "Could not find JSON in Gemini response", "Unknown", "Unknown", "Unknown")]
+
+            else:
+                print(f"Gemini response has no parts: {response.result}")
+                return [("UNKNOWN", "Gemini response has no parts", "Unknown", "Unknown", "Unknown")]
+        else:
+            print(f"Gemini returned no candidates: {response.result}")
+            return [("UNKNOWN", "Gemini returned no candidates", "Unknown", "Unknown", "Unknown")]
+
     except Exception as e:
         print(f"Error calling Gemini API: {e}")
-        return "UNKNOWN", "Error calling Gemini API"
+        return [("UNKNOWN", "Error calling Gemini API", "Unknown", "Unknown", "Unknown")]
 
 def write_to_csv(data, filename):
-    """Writes the extracted vulnerability data to a CSV file, 
+    """Writes the extracted vulnerability data to a CSV file,
     avoiding duplicates and using Gemini for categorization.
     """
-    fieldnames = ['id', 'title', 'description', 'vendor', 'published', 
-                  'cvss', 'cvss_score', 'severity', 'attack_vector', 
-                  'cwe_category', 'cwe_explanation', 'source']
-    existing_vulnerabilities = set()
+    fieldnames = ['id', 'title', 'description', 'vendor', 'published',
+                  'cvss_score', 'severity', 'cwe_category', 'cwe_explanation', 'cause', 'impact', 'source']
+    existing_vulnerabilities = {}
 
+    # Tente ler vulnerabilidades existentes do arquivo
     try:
-        with open(filename, 'r', encoding="utf-8") as csvfile:
+        with open(filename, 'r', encoding="latin1") as csvfile:  # Use latin1 aqui
             reader = csv.DictReader(csvfile)
-            for row in reader:
-                existing_vulnerabilities.add(row['id'])
+            # Verificar se o arquivo está vazio
+            if reader.fieldnames:
+                for row in reader:
+                    # Lidar com a possibilidade da chave 'id' não existir
+                    try:
+                        existing_vulnerabilities[row['title']] = row['id'] 
+                    except KeyError:
+                        print(f"Aviso: A linha não possui a chave 'id': {row}")
+                        # existing_vulnerabilities[row['title']] = "ID_DESCONHECIDO"
     except FileNotFoundError:
         pass
 
-    with open(filename, 'a', newline='', encoding="utf-8") as csvfile:
+    with open(filename, 'a', newline='', encoding="utf-8") as csvfile:  # UTF-8 para a escrita
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Escrever o cabeçalho apenas se o arquivo estiver vazio
         if len(existing_vulnerabilities) == 0:
             writer.writeheader()
+
         for item in data:
-            if item['id'] not in existing_vulnerabilities:
-                # Use Gemini to categorize the vulnerability
-                item['cwe_category'], item['cwe_explanation'] = categorize_vulnerability(item['description'])
-                writer.writerow(item)
-                existing_vulnerabilities.add(item['id'])
+            # Verificar se a vulnerabilidade já existe com base no título
+            if item['title'] not in existing_vulnerabilities:
+                vulnerability_data = categorize_vulnerability(item['description'])
+                # Filtrar vulnerabilidades que não são referentes a DDS e aos vendors especificados
+                if any(vendor.lower() in item['description'].lower() for vendor in VENDORS):
+                    for cwe_category, cwe_explanation, vendor, cause, impact in vulnerability_data:
+                        if vendor.strip().upper() != 'UNKNOWN':
+                            # Atribuir os valores ao dicionário 'item'
+                            item['cwe_category'] = cwe_category
+                            item['cwe_explanation'] = cwe_explanation
+                            item['vendor'] = vendor
+                            item['cause'] = cause
+                            item['impact'] = impact
+
+                            writer.writerow(item)
+                            existing_vulnerabilities[item['title']] = item['id'] 
 
 def main():
     """Orchestrates the vulnerability search and data extraction process."""
     all_vulnerabilities = []
-    
-    # --- Vulners ---
-    for vendor in VENDORS:
-        skip = 0 
-        while True:
-            data = get_vulners_data(f'"{vendor}"', FIELDS, skip) # formatted query for each vendor
+    search_query = ' OR '.join([f'"{vendor}"' for vendor in VENDORS]) + ' "data distribution service"'
+    skip = 0
+    while True:
+        data = get_vulners_data(search_query, FIELDS, skip)
+        # Verificar se 'data' não é None
+        if data is not None:
             new_vulnerabilities = [extract_vulners_data(cve) for cve in data['data']['search']]
             all_vulnerabilities.extend(new_vulnerabilities)
             total = data['data']['total']
-            skip += len(data['data']['search'])  # Use actual returned results for pagination
+            skip += len(data['data']['search'])
             if skip >= total:
                 break
+        else:
+            print(f"Erro: A API do Vulners retornou None para a consulta: {search_query}")
+            break
 
-    # Write to CSV, avoiding duplicates and using Gemini
     write_to_csv(all_vulnerabilities, CSV_OUTPUT_FILE)
     print(f"Vulnerability data saved to {CSV_OUTPUT_FILE}")
 
